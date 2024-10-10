7a25778276362507acab53d87982755386b5d8fb
Trần quang Phát <128336201+qphat@users.noreply.github.com>
2024-10-10 16:18:25 +0700

    Commit

---
+++ src/test/java/com/koomi/treadingplatfrom/TreadingPlatfromApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koomi.treadingplatfrom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreadingPlatfromApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
